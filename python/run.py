86d439f9beeffbac3c49440f525cd029688efccb
Gina Yuan <gina.yuan@gmail.com>
2020-08-31 13:25:53 -0700

    Python example has explicit file operations

--- python/run.py
+++ python/run.py
@@ -1,2 +1,11 @@
-import sys
-print(sys.path)
+import os
+print('create/write')
+with open('python/tmp0.txt', 'w+') as f:
+    f.write('apple')
+with open('python/tmp1.txt', 'w+') as f:
+    f.write('banana')
+print('rename')
+os.rename('python/tmp1.txt', 'python/tmp2.txt')
+print('delete')
+os.remove('python/tmp0.txt')
+
